b79ba854203d1f5617ece5b2c0a8b89a3bd7579a
DaRon-HomePC <max99124@gmail.com>
2025-08-27 21:04:06 +0800

    add : 建立 view 及 router 設定

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,31 @@
-import { createRouter, createWebHistory } from 'vue-router'
+// import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'//因為github page 或是靜態伺服器上，建議用#錨點類型
+import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  history: createWebHashHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView,
+    },
+    {
+      path: '/signin',
+      name: 'signin',
+      component: HomeView,
+    },
+    {
+      path: '/signup',
+      name: 'signup',
+      component: () => import('../views/SignUpView.vue'),
+    },
+    {
+      path: '/todoList',
+      name: 'todoList',
+      component: () => import('../views/TodoListView.vue'),
+    }
+  ],
 })
 
 export default router
